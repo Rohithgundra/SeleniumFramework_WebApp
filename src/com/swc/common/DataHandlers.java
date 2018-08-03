66332a1057e5b3c2486f926547d62ccb41392406
sritaj <sritajp@gmail.com>
2018-08-03 18:48:38 +0530

    Added ReadExcel generic method

--- src/com/swc/common/DataHandlers.java
+++ src/com/swc/common/DataHandlers.java
@@ -4,6 +4,15 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+
+
+
 public class DataHandlers {
 	
 	public static String getDataFromPropertyFile(String filename, String key) {
@@ -17,6 +26,26 @@ public class DataHandlers {
 			data = (String)prop.get(key);
 			
 		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return data;
+	}
+	
+	
+	public static String readExcel(String filepath, String sheetName, int rowNum, int cellIndex) {
+		
+		String data = null;
+		try {
+			File f = new File(filepath);
+			FileInputStream fis = new FileInputStream(f);
+			Workbook wb = WorkbookFactory.create(fis);
+			Sheet sh = wb.getSheet(sheetName);
+			Row rw = sh.getRow(rowNum);
+			Cell c = rw.getCell(cellIndex);
+			c.setCellType(Cell.CELL_TYPE_STRING);
+			data = c.getStringCellValue();
+			
+		}catch(Exception e) {
 			e.printStackTrace();
 		}
 		return data;
