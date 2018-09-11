3809c320868a62584a907a2b410165d9fc1f15b8
sritaj <sritajp@gmail.com>
2018-09-11 18:38:48 +0530

    Added Interface to keep all messages, titles and other texts for validating the UI interface

---
+++ src/com/swc/pompages/ReleavantData.java
@@ -0,0 +1,19 @@
+package com.swc.pompages;
+
+public interface ReleavantData {
+
+	 String expectedPageTitle = "Log in";
+	 String expectedHeader = "UnizenSWM Sol.";
+	 String expectedSubHeader = "Please Sign-in";
+	 String expectedUsernamePlaceholder = "Username";
+	 String expectedUsernameAttribute = "text";
+	 String expectedPasswordAttribute = "password";
+	 String expectedPasswordPlaceholder = "Password";
+	 String expectedInvalidCredentialsErrorMsg = "Invalid Username/Password !";
+	 String expectedInvalidUsernameErrorMsg = "Invalid Username/Password !";
+	 String expectedInvalidPasswordErrorMsg = "Invalid Username/Password !";
+	 String expectedBlankCredentialsErrorMsg = "Please enter Username!";
+	 String expectedBlankPasswordErrorMsg = "Please enter Password!";
+	 String expectedDashboardTitle = "Water";
+	  
+}
