e8b16cb38c6792f7e206cca7a7715ec1c45cf21b
sritaj <sritajp@gmail.com>
2018-09-14 19:38:18 +0530

    Added sample testscripts for manipulating User Configuration elements

---
+++ src/com/swc/testscripts/UserConfigTest.java
@@ -0,0 +1,38 @@
+package com.swc.testscripts;
+
+import org.openqa.selenium.WebDriver;
+
+import com.swc.common.TestConfiguration;
+import com.swc.pompages.SWC_Dashboard;
+import com.swc.pompages.SWC_LoginScreen;
+import com.swc.pompages.SWC_UserSubscription;
+
+public class UserConfigTest {
+
+	
+public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = null;
+		SWC_LoginScreen sl;
+		SWC_Dashboard sd;
+		SWC_UserSubscription us;
+		driver = TestConfiguration.getInstance();
+		sl = new SWC_LoginScreen(driver);
+		sd = new SWC_Dashboard(driver);
+		us = new SWC_UserSubscription(driver);
+		Thread.sleep(1000);
+		sl.UsernameTextfield().sendKeys("uswm");
+		sl.PasswordTextfield().sendKeys("6");
+		sl.SignInButton().click();
+		
+		sd.UserMgmtLink().click();
+		us.UserSubscriptionLink().click();
+		System.out.println(us.UserSubTitle());
+		System.out.println(us.UserSubTitle());
+		us.CreateUserButton().click();
+		us.HandleAlert();
+		
+		
+ }
+
+}
