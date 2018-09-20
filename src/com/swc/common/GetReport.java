9390ba342c5d7a35dc95a41e8e64a93e863dfed3
sritaj <sritajp@gmail.com>
2018-09-20 18:24:18 +0530

    Added more information for ExtentReports, failTestException method to get exception for failed test cases, minute changes

--- src/com/swc/common/GetReport.java
+++ src/com/swc/common/GetReport.java
@@ -3,54 +3,71 @@ package com.swc.common;
 import java.io.IOException;
 
 
+
 import org.openqa.selenium.WebDriver;
+
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
+
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 
 public class GetReport implements RelativePath {
 	
 	static ExtentHtmlReporter htmlReporter;
 	static ExtentReports extent;
 	static ExtentTest test;
-
-	
 	WebDriver driver;
 	
 	
 	public static void initializeReport() {
 		
 		htmlReporter = new ExtentHtmlReporter(extentReports_path);
+		htmlReporter.config().setReportName("Automation Test Report");
 		htmlReporter.setAppendExisting(true);
 		extent = new ExtentReports();
 		extent.attachReporter(htmlReporter);
 		extent.setSystemInfo(System.getProperty("os.name"), System.getProperty("os.version"));
+		extent.setSystemInfo("Browser Type", DataHandlers.getDataFromPropertyFile("browser"));
+		extent.setSystemInfo("Java Version", System.getProperty("java.version"));
 //		extent.loadConfig(new File(".\\extent-config.xml"));
-		
-		
+				
 	}
 	
-	public static void startTestExecution(String classname) {
-		
-		test = extent.createTest(classname);
+	public static void startTestExecution(String testName, String description) {
+
+		test = extent.createTest(testName, description);
 		
 	}
 	
 	public static void passTest(String testName) {
 		
-		test.pass("Test Case is passed " + testName);
+		test.pass(testName + " Test Case is PASSED");
+		
+	}
+	
+	public static void skipTest(String testName) {
+		
+		test.skip(testName + " Test Case is SKIPPED");
+		
 	}
 	
 	public static void failTest(String testName, String screenshot) throws IOException {
 		
-		test.fail("Following Test Case is failed  " + testName, MediaEntityBuilder.createScreenCaptureFromPath(screenshot).build());
+		test.fail(testName + " Test Case is FAILED", MediaEntityBuilder.createScreenCaptureFromPath(screenshot).build());
+		
+	}
 	
+	public static void failTestException(Throwable throwable) {
+		
+		test.fail(throwable);
 	}
 	
 	public static void closeReport() {
 		
 		extent.flush();
 	}
+
 }
